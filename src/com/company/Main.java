d4c9e7991621e050a49ae97e9510a2c5f30fae2d
KC210253 <kerrcameron@hotmail.com>
2022-01-10 10:09:08 +0000

    objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.library.libraryFunctions;
+import com.company.objects.dog;
 
 public class Main {
 
@@ -9,6 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
         int intInput = libraryFunctions.getInt("type a number");
+
+        dog myDog = new dog(6, "dog","big dog", 6,12.5, "dog food", 0.7, "monday");
 
     }
 
